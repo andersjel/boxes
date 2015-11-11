555f4b3f1cac02e6537dd9431de430472459b180
Anders Jellinggaard <anders.jel@gmail.com>
2015-11-11 23:33:51 +0100

    Documenting symmath.system.

--- src/symmath/system.py
+++ src/symmath/system.py
@@ -2,9 +2,22 @@
 symmath.system
 --------------
 
+.. doctest::
+  :hide:
+
+  >>> from symmath import *
+
 .. autoclass:: System
   :members:
   :undoc-members:
+
+  .. attribute:: facts
+
+    A dictionary mapping symbol names to equivalent expressions. Each value in
+    this dictionary should not contain any references to the symbols used as
+    keys. Consider this attribute read-only, and use :func:`equate` to add a
+    fact instead of modifying this dictionary.
+
 """
 from symmath.expr import Expr
 from collections import abc
@@ -16,6 +29,9 @@ class System:
     self.facts = {}
 
   def rewrite(self, expr):
+    """
+    Substitue all known :attr:`facts` into *expr*.
+    """
     for symbol in list(expr.terms):
       try:
         fact = self.facts[symbol]
@@ -24,16 +40,66 @@ class System:
       expr.substitue(symbol, fact)
 
   def simplify(self, expr):
+    """
+
+    Same as :func:`rewrite`, but returns the simplified expression instead of
+    modifying *expr*.
+
+    """
     expr = Expr(expr)
     self.rewrite(expr)
     return expr
 
   def eval(self, val):
+    """
+
+    Substitue all known facts into *val*, recursing into structures that support
+    it. :func:`eval` calls :func:`symmath.expr.Expr.scalar` to eliminate all
+    expressions. This method raises an exception if the system is not fully
+    solved.
+
+    Classes supporting this function define the method :func:`_symmath_eval`,
+    which takes a single argument: the :func:`eval` method as a bound method,
+    and should return a fully evaluated copy of itself.
+
+    """
     if hasattr(val, '_symmath_eval'):
       return val._symmath_eval(self.eval)
     return self.simplify(val).scalar()
 
   def equate(self, a, b):
+    """
+
+    Eliminate one symbol from the system by using the equation: *a = b*.
+    This adds a fact to :attr:`facts` and simplifies all existing facts.
+
+    Classes can define :func:`_symmath_equate` to customize what it means
+    objects of that class to be equal to something else.
+
+    .. doctest::
+
+      >>> class Vector2D:
+      ...   def __init__(self, x, y):
+      ...     self.x = x
+      ...     self.y = y
+      ...
+      ...   def _symmath_equate(self, f, other):
+      ...     f(self.x, other.x)
+      ...     f(self.y, other.y)
+
+      >>> x = sym('x')
+      >>> y = sym('y')
+      >>> a = Vector2D(x + 5, 2*y)
+      >>> b = Vector2D(4*y, 3*x)
+
+      >>> system = System()
+      >>> system.equate(a, b)
+      >>> system.eval(x)
+      1.0
+      >>> system.eval(y)
+      1.5
+
+    """
     if hasattr(a, '_symmath_equate'):
       a._symmath_equate(self.equate, b)
       return
